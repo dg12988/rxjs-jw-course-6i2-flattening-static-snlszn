71c6c9c560d3bed3b059ac3e57c01d2bdb6ee73e
dg12988 <doug.goldberg@me.com>
2021-08-12 15:56:29 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -4,4 +4,6 @@ import { Observable } from "rxjs";
 const source$ = new Observable(subscriber => {
   setTimeout(() => subscriber.next('A'), 2000);
   setTimeout(() => subscriber.next('B'), 5000);
-})
+});
+console.log('App started');
+source$.subscribe(value => console.log(value));
