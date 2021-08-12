c4e0caacfc800aa64a6dff833ea145d61281d046
dg12988 <doug.goldberg@me.com>
2021-08-12 16:00:53 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -1,9 +1,15 @@
-import { Observable } from "rxjs";
+import { Observable, of } from "rxjs";
+import { concatMap } from "rxjs/operators";
 
 
 const source$ = new Observable(subscriber => {
   setTimeout(() => subscriber.next('A'), 2000);
   setTimeout(() => subscriber.next('B'), 5000);
 });
+
+//use flattening operator to create new Observable, reaches next and concatMap is run
 console.log('App started');
-source$.subscribe(value => console.log(value));
+source$.pipe(
+  concatMap(value => of(1, 2))
+).subscribe(value => console.log(value));
+
